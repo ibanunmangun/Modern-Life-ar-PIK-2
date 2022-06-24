6ad9d0f383588c0a1f9a73c6c74a63044e3c7982
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 22:24:06 +0800

    Add files via upload

---
+++ data/point_2.js
@@ -0,0 +1,11 @@
+var json_point_2 = {
+"type": "FeatureCollection",
+"name": "point_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Cove at Batavia", "desc": "Cove at Batavia PIK, Indonesia", "X": "106.741447", "Y": "-6.0886957" }, "geometry": { "type": "Point", "coordinates": [ 106.7414474, -6.0886957 ] } },
+{ "type": "Feature", "properties": { "name": "Urban Farm", "desc": "Urban Farm PIK, Kawasan Pantai Maju, Jl. The Golf Island Boulevard Kel, Kamal Muara, Kec. Penjaringan, Daerah Khusus Ibukota Jakarta 14460, Indonesia", "X": "106.742980", "Y": "-6.0907529" }, "geometry": { "type": "Point", "coordinates": [ 106.7429807, -6.0907529 ] } },
+{ "type": "Feature", "properties": { "name": "San Antonio Beach", "desc": "San Antonio Beach PIK 2, WP4Q+JR4, Kamal Muara, North Jakarta City, Jakarta, Indonesia", "X": "106.739584", "Y": "-6.093495" }, "geometry": { "type": "Point", "coordinates": [ 106.7395843, -6.093495 ] } },
+{ "type": "Feature", "properties": { "name": "Pantjoran Chinatown", "desc": "Pantjoran Chinatown PIK, Unnamed Road, 14460, Indonesia", "X": "106.744619", "Y": "-6.0906997" }, "geometry": { "type": "Point", "coordinates": [ 106.7446195, -6.0906997 ] } }
+]
+}
